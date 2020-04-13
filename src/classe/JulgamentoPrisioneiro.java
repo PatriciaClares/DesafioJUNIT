ba38ef8525e3d24b10e9cf636242ceda91a00129
PatriciaClares <dev.patricia.c@gmail.com>
2020-04-13 11:39:14 -0300

    corrigindo pena inocencia

--- src/classe/JulgamentoPrisioneiro.java
+++ src/classe/JulgamentoPrisioneiro.java
@@ -1,23 +1,23 @@
 package classe;
 
 public class JulgamentoPrisioneiro {
-	private int PENA_INOCENCIA = 13;
+	private int PENA_INOCENCIA = 0;
 	private int PENA_CONDENACAO_MUTUA = 15;
 	private int PENA_CONDENACAO_INDIVIDUAL = 10;
 	private int PENA_CONDENACAO_CUMPLICES = 11;
 
 	public int calculaPena(String respostaPrisioneiroA, String respostaPrisioneiroB) {
-		if (respostaPrisioneiroA == Resposta.DELACAO) {
-			if (respostaPrisioneiroB == Resposta.DELACAO) {
+		if (respostaPrisioneiroA == "Culpado") {
+			if (respostaPrisioneiroB == "Culpado") {
 				return PENA_CONDENACAO_MUTUA;
 			} else {
 				return PENA_INOCENCIA;
 			}
 		} else {
-			if (respostaPrisioneiroB == Resposta.DELACAO) {
+			if (respostaPrisioneiroB == "Culpado") {
 				return PENA_CONDENACAO_INDIVIDUAL;
 			} else {
-				return PENA_CONDENACAO_CUMPLICES;
+				return PENA_INOCENCIA;
 			}
 		}
 	}
